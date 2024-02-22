399d30bccf9e31f1270c4140972dde033f89440c
Obi William <williamobi818@gmail.com>
2024-02-22 10:50:40 +0100

    user_authentication_service these update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,8 @@
 """ Flask module
 """
 from auth import Auth
-from flask import Flask, jsonify, request, make_response, abort, redirect, \
-    url_for
+from flask import Flask, jsonify, request, make_response, abort,\
+    redirect, url_for
 from sqlalchemy.orm.exc import NoResultFound
 
 AUTH = Auth()
@@ -75,23 +75,24 @@ def logout():
 def profile():
     """Profile route"""
     session_id = request.cookies.get('session_id')
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
-        abort(403)
-    else:
-        return jsonify({"email": user.email}), 200
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+    abort(403)
 
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token():
     """get reset password token"""
     email = request.form.get('email')
-    uu_id = AUTH.get_reset_password_token(email)
-    if uu_id:
-        return jsonify({"email": email,
-                        "reset_token": uu_id}), 200
-    else:
-        abort(403)
+    if email:
+        try:
+            reset_token = AUTH.get_reset_password_token(email)
+            return (
+                jsonify({"email": email, "reset_token": reset_token}), 200)
+        except ValueError:
+            abort(403)
 
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
@@ -100,12 +101,13 @@ def update_password():
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email,
-                        "message": "Password updated"}), 200
-    except ValueError as e:
-        return jsonify({"error": str(e)})
+    if email and reset_token and new_password:
+        try:
+            AUTH.update_password(reset_token, new_password)
+            return (jsonify({"email": email,
+                             "message": "Password updated"}), 200)
+        except NoResultFound:
+            abort(403)
 
 
 if __name__ == "__main__":
